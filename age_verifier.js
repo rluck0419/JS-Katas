9c1e5f48c1047ade12e12c8d2fd80bc7dc2c6d75
Robert Luckfield <rluck0419@gmail.com>
2016-07-26 13:26:04 -0500

    age verifier takes an array now (js)

--- age_verifier.js
+++ age_verifier.js
@@ -1,7 +1,12 @@
-var ageVerifier = function(age) {
-  if (age < 21) {
-    console.log("too young");
-  } else {
-    console.log("proceed");
-  }
+var ageVerifier = function(ages) {
+    ages.forEach(function (age) {
+        if (age < 21) {
+            console.log("too young");
+        } else {
+            console.log("proceed");
+        }
+    });
 }
+
+var arr_of_ages = [10, 20, 40, 15, 30];
+ageVerifier(arr_of_ages);
