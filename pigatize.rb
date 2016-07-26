f0675c7950dc75ddeda56769ceb4ab867085e509
Robert Luckfield <rluck0419@gmail.com>
2016-07-26 13:51:45 -0500

    pigatize for ruby

--- pigatize.rb
+++ pigatize.rb
@@ -1,3 +1,15 @@
 def starts_with_vowel?(text)
-  text[0] == /[aeiou]/i
+  /[aeiou]/i === text[0]
 end
+
+def pigatize(text)
+  if starts_with_vowel?(text)
+    puts "#{text}way"
+  else
+    puts "#{text[1..-1]}#{text[0]}ay"
+  end
+end
+
+pigatize("stop")
+pigatize("go")
+pigatize("apple")
