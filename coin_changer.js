de8b3f5e9c013fd67096965c01e4173e52468e4b
Robert Luckfield <rluck0419@gmail.com>
2016-07-26 14:31:05 -0500

    coin changer for js

---
+++ coin_changer.js
@@ -0,0 +1,19 @@
+get_change = function (n) {
+    var coins = [25, 10, 5, 1]
+    var result = []
+    while (n > 0) {
+        coins.forEach(function (coin) {
+            for(var i=0;i<5;i++) {
+                if (n - coin >= 0) {
+                    result.push(coin);
+                    n -= coin;
+                };
+            };
+        });
+    };
+    console.log(result.join(" "));
+};
+
+get_change(50);
+get_change(99);
+get_change(105);
