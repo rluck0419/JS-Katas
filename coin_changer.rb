95e03d0669c432056a8ca8d72cd7ce9c9b0b3769
Robert Luckfield <rluck0419@gmail.com>
2016-07-26 14:25:28 -0500

    coin changer for rb

---
+++ coin_changer.rb
@@ -0,0 +1,19 @@
+def get_change(n)
+  coins = [25,10,5,1]
+  result = []
+  while n > 0
+    coins.each do |coin|
+      4.times do
+        if n - coin >= 0
+          result << coin
+          n -= coin
+        end
+      end
+    end
+  end
+  puts result.join(" ")
+end
+
+get_change(50)
+get_change(99)
+get_change(105)
