354ab42bc7fd515c184fbc4f5a6cb40871fc7ace
Robert Luckfield <rluck0419@gmail.com>
2016-07-26 13:27:06 -0500

    age verifier now takes arrays for both js and rb

--- age_verifier.rb
+++ age_verifier.rb
@@ -1,7 +1,12 @@
-def age_verifier(age)
-  if age < 21
-    puts "too young"
-  else
-    puts "proceed"
+def age_verifier(ages)
+  ages.each do |age|
+    if age < 21
+      puts "too young"
+    else
+      puts "proceed"
+    end
   end
 end
+
+arr_of_ages = [10,15,25,30,35]
+age_verifier(arr_of_ages)
